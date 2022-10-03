5fcfd1efdc70e0f3f943d08bd7d51f0a4d8d5342
onana <onanadonald@gmail.com>
2022-10-03 09:32:46 +0100

    messages

--- Camset/settings.py
+++ Camset/settings.py
@@ -132,7 +132,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = "/staticfiles/img/"
 MEDIA_ROOT = [os.path.join(BASE_DIR, 'staticfiles')]    
 # Default primary key field type
