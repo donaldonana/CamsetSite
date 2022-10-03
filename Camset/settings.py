d1792d8d3fb48822a83f1c4e60392b54951862a9
onana <onanadonald@gmail.com>
2022-10-03 09:19:02 +0100

    messages

--- Camset/settings.py
+++ Camset/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+# DEBUG = True
+
 
 ALLOWED_HOSTS = ["*"]
 
@@ -129,7 +131,10 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'Camset/static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = "/staticfiles/img/"
+MEDIA_ROOT = [os.path.join(BASE_DIR, 'staticfiles')]    
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
