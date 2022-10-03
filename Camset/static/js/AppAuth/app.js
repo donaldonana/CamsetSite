5d7ddf7d31b0dac3f2f4dae0a0199332e2cd0f0d
onana <onanadonald@gmail.com>
2022-10-03 02:08:08 +0100

    messages

---
+++ Camset/static/js/AppAuth/app.js
@@ -0,0 +1,31 @@
+
+
+$(document).ready(function () {
+
+
+	 })
+
+
+
+ function login()
+              {
+
+
+                   window.setTimeout(function () {
+                            window.location.href = 'login';
+                        }, 0);
+
+
+              };
+
+
+ function register()
+              {
+
+
+                   window.setTimeout(function () {
+                            window.location.href = 'register';
+                        }, 0);
+
+
+              };
