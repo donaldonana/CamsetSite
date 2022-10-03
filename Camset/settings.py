a189057ed9c8338cffcfcb27c6d03c1d39b852d7
onana <onanadonald@gmail.com>
2022-10-03 09:39:11 +0100

    messages

--- Camset/settings.py
+++ Camset/settings.py
@@ -132,10 +132,12 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_URL = "/staticfiles/img/"
-MEDIA_ROOT = [os.path.join(BASE_DIR, 'staticfiles')]    
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# MEDIA_URL = "/staticfiles/img/"
+# MEDIA_ROOT = [os.path.join(BASE_DIR, 'staticfiles')]    
 # Default primary key field type
+
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
